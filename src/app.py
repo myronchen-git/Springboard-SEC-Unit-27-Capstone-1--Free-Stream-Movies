bae4c3aba1bdeb9184199cd85b8f7190fa9ea9b0
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-07-31 13:38:14 -0700

    chore: set up folder and file structure
    The requirements file has been split up to have different dependencies
    installed in a production and development environment.  There is still
    a requirements.txt in the root directory for cases where a hoster only
    reads that file in the root.

---
+++ src/app.py
@@ -0,0 +1,44 @@
+import os
+
+from flask import Flask
+
+from src.models.models import connect_db, db
+
+# ==================================================
+
+CURR_USER_KEY = "curr_user"
+
+
+def create_app(db_name, testing=False):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        os.environ.get('DATABASE_URL', f'postgresql://postgres@localhost/{db_name}'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+    if not testing:
+        app.config['SQLALCHEMY_ECHO'] = False
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+        app.config['DEBUG_TB_HOSTS'] = ["dont-show-debug-toolbar"]
+
+    else:
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_ECHO'] = True
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+        # debug = DebugToolbarExtension(app)
+
+    # --------------------------------------------------
+
+    return app
+
+# ==================================================
+
+
+if __name__ == "__main__":
+    app = create_app("freestreammovies")
+    connect_db(app)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
