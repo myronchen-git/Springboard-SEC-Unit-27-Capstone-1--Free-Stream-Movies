a93b7f677dcaff7ec3c3c19fea504e8e2e5c55c3
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-08-19 15:22:32 -0700

    feat: create movie posters methods
    Created a method, get_movie_posters, that will retrieve a list of
    MoviePosters when given movie_ids, types, and sizes.  This is currently
    used for the API side, to allow for asynchronously getting the posters,
    but can be used for anything requiring a list of MoviePoster objects.
    
    Create a method, convert_list_to_dict, to convert a list of MoviePoster
    objects to a dictionary, so that it can be returned in an HTTP response.
    
    Also added in Enums for movie poster types and sizes, since those have
    limited values and will not change.

--- src/models/movie_poster.py
+++ src/models/movie_poster.py
@@ -1,3 +1,7 @@
+from enum import StrEnum
+from typing import Self
+
+from src.exceptions.UnrecognizedValueError import UnrecognizedValueError
 from src.models.common import db
 
 # ==================================================
@@ -31,7 +35,64 @@ class MoviePoster(db.Model):
 
     movie = db.relationship('Movie', back_populates='movie_posters')
 
+    # currently only storing vertical posters
+    Types = StrEnum('Types', {'VERTICAL_POSTER': 'verticalPoster'})
+    VerticalSizes = StrEnum('VerticalSizes', ['W240', 'W360', 'W480', 'W600', 'W720'])
+
     def __repr__(self) -> str:
         """Show info about movie poster."""
 
         return "{}({!r})".format(self.__class__.__name__, self.__dict__)
+
+    @classmethod
+    def get_movie_posters(cls, movie_ids: list[str], types: list[str], sizes: list[str]) -> list[Self]:
+        """
+        Retrieves a dictionary of movie posters for specified movies, types, and sizes.
+        Queries the database.
+
+        @param {list[str]} movie_ids - Movie IDs of the posters to retrieve.
+        @param {list[str]} types - Movie poster types, such as verticalPoster.
+        @param {list[str]} sizes - Movie poster sizes, such as w240.
+        @returns {movie_id: {type: {size: link}}}.
+        """
+
+        for type in types:
+            if type not in cls.Types:
+                supported_types = tuple(type.value for type in cls.Types)
+                raise UnrecognizedValueError(
+                    f'Movie poster type(s) is unrecognized.  Supported types are {supported_types}.')
+
+        for size in sizes:
+            if size not in cls.VerticalSizes:
+                supported_sizes = tuple(size.value for size in cls.VerticalSizes)
+                raise UnrecognizedValueError(
+                    f'Movie poster size(s) is unrecognized.  Supported sizes are {supported_sizes}.')
+
+        return db.session\
+            .query(MoviePoster)\
+            .filter(
+                MoviePoster.movie_id.in_(movie_ids),
+                MoviePoster.type.in_(types),
+                MoviePoster.size.in_(sizes)
+            )\
+            .all()
+
+    @classmethod
+    def convert_list_to_dict(cls, movie_posters: list[Self]) -> dict:
+        """
+        Converts a list of MoviePosters to a dict {movie_id: {type: {size: link}}}.
+
+        @param {list[MoviePoster]} movie_posters - A list of MoviePoster objects.
+        @returns {movie_id: {type: {size: link}}}.
+        """
+
+        output = {}
+        for movie_poster in movie_posters:
+            movie_posters_of_movie_id = output.get(movie_poster.movie_id, {})
+            movie_posters_of_movie_id_and_type = movie_posters_of_movie_id.get(movie_poster.type, {})
+
+            movie_posters_of_movie_id_and_type.update({movie_poster.size: movie_poster.link})
+            movie_posters_of_movie_id.update({movie_poster.type: movie_posters_of_movie_id_and_type})
+            output.update({movie_poster.movie_id: movie_posters_of_movie_id})
+
+        return output
---
+++ tests/models/test_movie_poster.py
@@ -0,0 +1,239 @@
+import sys
+from os.path import abspath, dirname, join
+
+# Adds root folder as a working directory.
+# This is needed so that imports can be found.
+root_dir = abspath(join(dirname(__file__), '../../'))  # nopep8
+sys.path.append(root_dir)  # nopep8
+
+# --------------------------------------------------
+
+from unittest import TestCase
+from unittest.mock import call, patch
+
+from src.app import create_app
+from src.exceptions.UnrecognizedValueError import UnrecognizedValueError
+from src.models.common import connect_db, db
+from src.models.movie import Movie
+from src.models.movie_poster import MoviePoster
+from tests.util import movie_generator, movie_poster_generator
+
+# ==================================================
+
+app = create_app("freestreammovies_test", testing=True)
+connect_db(app)
+app.app_context().push()
+
+db.drop_all()
+db.create_all()
+
+# --------------------------------------------------
+
+
+class MoviePosterUnitTestsGetMoviePosters(TestCase):
+    """Unit tests for MoviePoster.get_movie_posters()."""
+
+    def tearDown(self):
+        db.session.rollback()
+
+    @patch('src.models.movie_poster.MoviePoster.size', autospec=True)
+    @patch('src.models.movie_poster.MoviePoster.type', autospec=True)
+    @patch('src.models.movie_poster.MoviePoster.movie_id', autospec=True)
+    @patch('src.models.movie_poster.db', autospec=True)
+    def test_successful_query_and_data_return(
+        self,
+        mock_db,
+        mock_movie_poster_movie_id,
+        mock_movie_poster_type,
+        mock_movie_poster_size
+    ):
+        """Tests that a list of MoviePosters is returned when given valid arguments."""
+
+        # Arrange
+        movie_ids = ('123',)
+        types = ('verticalPoster',)
+        sizes = ('w240',)
+
+        movie_posters = movie_poster_generator(movie_ids)
+
+        mock_db.session.query.return_value.filter.return_value.all.return_value = movie_posters
+
+        expected_call = call.session.query(MoviePoster).filter(
+            mock_movie_poster_movie_id.in_(movie_ids),
+            mock_movie_poster_type.in_(types),
+            mock_movie_poster_size.in_(sizes)
+        ).all().call_list()
+
+        # Act
+        result = MoviePoster.get_movie_posters(movie_ids, types, sizes)
+
+        # Assert
+        self.assertEqual(result, movie_posters)
+
+        self.assertEqual(mock_db.mock_calls, expected_call)
+        mock_movie_poster_movie_id.in_.assert_called_with(movie_ids)
+        mock_movie_poster_type.in_.assert_called_with(types)
+        mock_movie_poster_size.in_.assert_called_with(sizes)
+
+    @patch('src.models.movie_poster.db', autospec=True)
+    def test_invalid_argument_for_type(self, mock_db):
+        """Passing an invalid type for an argument should throw an exception."""
+
+        # Arrange
+        movie_ids = ('123',)
+        types = ('verticalPoster', 'diagonalPoster')
+        sizes = ('w240',)
+
+        # Act / Assert
+        self.assertRaises(UnrecognizedValueError, MoviePoster.get_movie_posters, *(movie_ids, types, sizes))
+        mock_db.assert_not_called()
+
+    @patch('src.models.movie_poster.db', autospec=True)
+    def test_invalid_argument_for_size(self, mock_db):
+        """Passing an invalid size for an argument should throw an exception."""
+
+        # Arrange
+        movie_ids = ('123',)
+        types = ('verticalPoster')
+        sizes = ('w240', 'w9999')
+
+        # Act / Assert
+        self.assertRaises(UnrecognizedValueError, MoviePoster.get_movie_posters, *(movie_ids, types, sizes))
+        mock_db.assert_not_called()
+
+
+class MoviePosterUnitTestsConvertListToDict(TestCase):
+    """Unit tests for MoviePoster.convert_list_to_dict()."""
+
+    def test_convert_movie_posters_list_to_dict(self):
+        """Passing in a list[MoviePoster] should return a dictionary."""
+
+        # Arrange
+        movie_posters = [
+            MoviePoster(
+                movie_id='1',
+                type='verticalPoster',
+                size='w240',
+                link='www.example.com/image/1/w240'
+            ),
+            MoviePoster(
+                movie_id='1',
+                type='verticalPoster',
+                size='w360',
+                link='www.example.com/image/1/w360'
+            ),
+            MoviePoster(
+                movie_id='2',
+                type='verticalPoster',
+                size='w240',
+                link='www.example.com/image/2/w240'
+            ),
+        ]
+
+        expected_result = {
+            '1': {
+                'verticalPoster': {
+                    'w240': 'www.example.com/image/1/w240',
+                    'w360': 'www.example.com/image/1/w360'
+                }
+            },
+            '2': {
+                'verticalPoster': {
+                    'w240': 'www.example.com/image/2/w240',
+                }
+            }
+        }
+
+        # Act
+        result = MoviePoster.convert_list_to_dict(movie_posters)
+
+        # Assert
+        self.assertEqual(result, expected_result)
+
+    def test_convert_empty_movie_posters_list_to_empty_dict(self):
+        """Passing an empty list should return an empty dictionary."""
+
+        # Arrange
+        movie_posters = []
+
+        # Act
+        result = MoviePoster.convert_list_to_dict(movie_posters)
+
+        # Assert
+        self.assertEqual(result, {})
+
+
+class MoviePosterIntegrationTestsGetMoviePosters(TestCase):
+    """Integration tests for MoviePoster.get_movie_posters()."""
+
+    def setUp(self):
+        db.session.query(MoviePoster).delete()
+        db.session.query(Movie).delete()
+        db.session.commit()
+
+        movies = movie_generator(2)
+        db.session.add_all(movies)
+
+        movie_posters = tuple(movie_poster_generator(('0', '1')))
+        db.session.add_all(movie_posters)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_get_no_movie_posters(self):
+        """Tests that no movie posters are retrieved when there aren't any for a movie."""
+
+        # Arrange
+        movie_ids = ('3',)
+        types = ('verticalPoster',)
+        sizes = ('w240',)
+
+        # Act
+        result = MoviePoster.get_movie_posters(movie_ids, types, sizes)
+
+        # Assert
+        self.assertEqual(len(result), 0)
+
+    def test_get_one_movie_poster(self):
+        """Tests that one movie poster is retrieved for a specific movie ID, type, and size."""
+
+        # Arrange
+        movie_ids = ('0',)
+        types = ('verticalPoster',)
+        sizes = ('w240',)
+
+        # Act
+        result = MoviePoster.get_movie_posters(movie_ids, types, sizes)
+
+        # Assert
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].movie_id, movie_ids[0])
+        self.assertEqual(result[0].type, types[0])
+        self.assertEqual(result[0].size, sizes[0])
+
+    def test_get_many_movie_posters(self):
+        """Tests that multiple movie posters are retrieved when given multiple movie IDs, types, or sizes."""
+
+        # Arrange
+        movie_ids = ('0', '1')
+        types = ('verticalPoster',)
+        sizes = ('w240', 'w720')
+
+        # Act
+        result = MoviePoster.get_movie_posters(movie_ids, types, sizes)
+
+        # Assert
+        self.assertEqual(len(result), len(movie_ids) * len(types) * len(sizes))
+
+        result_movie_ids = {movie_poster.movie_id for movie_poster in result}
+        for movie_id in movie_ids:
+            self.assertIn(movie_id, result_movie_ids)
+
+        result_types = {movie_poster.type for movie_poster in result}
+        for type in types:
+            self.assertIn(type, result_types)
+
+        result_sizes = {movie_poster.size for movie_poster in result}
+        for size in sizes:
+            self.assertIn(size, result_sizes)
