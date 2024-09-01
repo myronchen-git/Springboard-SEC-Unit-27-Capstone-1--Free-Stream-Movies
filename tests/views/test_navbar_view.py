38c3f699477cbd2bf810a6af6228c2cd8a486cba
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-08-31 17:04:50 -0700

    refactor: replace hardcoded routes in navbar
    Routes were hardcoded in navbar.html.  For example, there were
    "/users/registration" and "/users/login" for the href values in the
    HTML.
    
    The hardcoded values are now changed using Flask's url_for(), so that
    routes can easily be changed.

--- tests/views/test_navbar_view.py
+++ tests/views/test_navbar_view.py
@@ -10,13 +10,17 @@ sys.path.append(root_dir)  # nopep8
 
 from unittest import TestCase
 
-from flask import render_template
+from flask import render_template, url_for
 
 from src.app import create_app
 
 # ==================================================
 
 app = create_app("freestreammovies_test", testing=True)
+app.config.update(
+    WTF_CSRF_ENABLED=False,
+    SERVER_NAME="localhost:5000"
+)
 app.app_context().push()
 
 # --------------------------------------------------
@@ -25,6 +29,14 @@ app.app_context().push()
 class NavbarViewTestCase(TestCase):
     """Tests the view for the navbar."""
 
+    @classmethod
+    def setUpClass(cls):
+        cls.home_url = url_for('home', _external=True)
+        cls.search_titles_url = url_for('search_titles', _external=True)
+        cls.register_user_url = url_for('register_user', _external=True)
+        cls.login_user_url = url_for('login_user', _external=True)
+        cls.logout_user_url = url_for('logout_user', _external=True)
+
     def test_display_common_navbar_elements(self):
         """Tests displaying the home button and search bar."""
 
@@ -32,8 +44,8 @@ class NavbarViewTestCase(TestCase):
         html = render_template('partials/navbar.html')
 
         # Assert
-        self.assertIn('href="/">Home</a>', html)
-        self.assertIn('action="/movies"', html)
+        self.assertIn(f'href="{self.home_url}">Home</a>', html)
+        self.assertIn(f'action="{self.search_titles_url}"', html)
         self.assertIn('placeholder="Search Movie Titles"', html)
 
     def test_display_navbar_when_not_logged_in(self):
@@ -46,9 +58,9 @@ class NavbarViewTestCase(TestCase):
         html = render_template('partials/navbar.html', current_user=current_user)
 
         # Assert
-        self.assertIn('href="/users/registration"', html)
-        self.assertIn('href="/users/login"', html)
-        self.assertNotIn('action="/users/logout"', html)
+        self.assertIn(f'href="{self.register_user_url}"', html)
+        self.assertIn(f'href="{self.login_user_url}"', html)
+        self.assertNotIn(f'action="{self.logout_user_url}"', html)
 
     def test_display_navbar_when_logged_in(self):
         """Tests displaying the profile and logout buttons."""
@@ -60,6 +72,6 @@ class NavbarViewTestCase(TestCase):
         html = render_template('partials/navbar.html', current_user=current_user)
 
         # Assert
-        self.assertIn('action="/users/logout"', html)
-        self.assertNotIn('href="/users/registration"', html)
-        self.assertNotIn('href="/users/login"', html)
+        self.assertIn(f'action="{self.logout_user_url}"', html)
+        self.assertNotIn(f'href="{self.register_user_url}"', html)
+        self.assertNotIn(f'href="{self.login_user_url}"', html)
